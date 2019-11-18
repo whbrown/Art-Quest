674c5962e0ef5d98afd8b8533ca3d54948ea0189
William Brown <hce@WmHB.local>
2019-11-18 12:28:28 +0100

    Initialize development branch

--- bin/seeds.js
+++ bin/seeds.js
@@ -7,36 +7,68 @@
 
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const fs = require('fs');
+const https = require('https');
 const User = require('../models/User');
 
 const bcryptSalt = 10;
 
-mongoose
-  .connect('mongodb://localhost/art-quest', { useNewUrlParser: true })
-  .then(x => {
-    console.log(
-      `Connected to Mongo! Database name: "${x.connections[0].name}"`
-    );
-  })
-  .catch(err => {
-    console.error('Error connecting to mongo', err);
+function download(url, filepath) {
+  const file = fs.createWriteStream(filepath);
+  const request = https.get(url, response => {
+    response.pipe(file);
   });
+}
+
+console.log(fs.pwd);
+// download(
+//   'https://media.githubusercontent.com/media/metmuseum/openaccess/master/MetObjects.csv',
+//   'full-met-collection.csv'
+// );
+
+// function download(url, filepath) {
+//   const fileStream = fs.createWriteStream(filepath);
+//   const deferred = Q.defer();
+
+//   fileStream
+//     .on('open', () => {
+//       https.get(url, res => {
+//         res.on('error', err => deferred.reject(err));
+//         res.pipe(fileStream);
+//       });
+//     })
+//     .on('error', err => deferred.reject(err))
+//     .on('finish', () => deferred.resolve(filepath));
+
+//   return deferred.promise;
+// }
+
+// mongoose
+//   .connect('mongodb://localhost/art-quest', { useNewUrlParser: true })
+//   .then(x => {
+//     console.log(
+//       `Connected to Mongo! Database name: "${x.connections[0].name}"`
+//     );
+//   })
+//   .catch(err => {
+//     console.error('Error connecting to mongo', err);
+//   });
 
 let users = [
   // TODO
 ];
 
-User.deleteMany()
-  .then(() => User.create(users))
-  .then(usersCreated => {
-    console.log(`${usersCreated.length} users created with the following id:`);
-    console.log(usersCreated.map(u => u._id));
-  })
-  .then(() => {
-    // Close properly the connection to Mongoose
-    mongoose.disconnect();
-  })
-  .catch(err => {
-    mongoose.disconnect();
-    throw err;
-  });
+// User.deleteMany()
+//   .then(() => User.create(users))
+//   .then(usersCreated => {
+//     console.log(`${usersCreated.length} users created with the following id:`);
+//     console.log(usersCreated.map(u => u._id));
+//   })
+//   .then(() => {
+//     // Close properly the connection to Mongoose
+//     mongoose.disconnect();
+//   })
+//   .catch(err => {
+//     mongoose.disconnect();
+//     throw err;
+//   });

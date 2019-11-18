07ec0c0a096607bbf02cc98e0cecfde869d553e7
fabianschu <48454910+fabianschu@users.noreply.github.com>
2019-11-18 15:33:18 +0100

    first test array retrieved with promise.all

--- scraper/scraper.js
+++ scraper/scraper.js
@@ -1,14 +1,40 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
+let testArray= [7984, 7994, 8029, 8007, 8025, 8030, 8031, 8018, 7999, 8016];
+let resultArray = []
+/*
+const testRetrieval = () => {
+  Promise.all(testArray.map(item => {
+    return getLocationDescription(item);
+  })).then(res => console.log(res)).catch(err => console.log(err))
+}
+*/
 
-
-const getLocationDescription = (objectLink) => {
-  axios.get(`${objectLink}`)
+/*
+const getOnDisplayItems = () => {
+  axios.get(`https://collectionapi.metmuseum.org/public/collection/v1/search?isOnView=true&q=*`)
+    .then(response => {
+      let OnDisplayItemIds = response.data.objectIDs;
+      console.log(OnDisplayItemIds);
+    })
+    .catch(err => {
+      console.log(err)
+    })
+}
+*/
+const getLocationDescription = (objectId) => {
+  return axios.get(`https://www.metmuseum.org/art/collection/search/${objectId}`)
       .then(response => {
         if(response.status === 200) {
           const html = response.data;
           const $ = cheerio.load(html);
-          console.log($(".artwork__intro__desc").text());
+          const description = $(".artwork__intro__desc").text()
+          const location =  $(".artwork__location--gallery").text()
+          return {
+            objectId: objectId,
+            location: location,
+            description: description.replace(/^\s+|\s+$/g, '')
+          }
         }  
       })
       .catch(err => {
@@ -16,4 +42,6 @@ const getLocationDescription = (objectLink) => {
       })
 }
 
-getLocationDescription(`https://www.metmuseum.org/art/collection/search/436524`);
+//getLocationDescription(`https://www.metmuseum.org/art/collection/search/436524`);
+//getOnDisplayItems()
+testRetrieval()

b92484883c7fab2163c15c41e980c35aee282f7c
fabianschu <48454910+fabianschu@users.noreply.github.com>
2019-11-18 17:14:41 +0100

    saving before using a setinterval() + count

--- scraper/scraper.js
+++ scraper/scraper.js
@@ -1,16 +1,32 @@
+// run test scraping with with 'node --http-parser=legacy scraper/scraper.js'
+const fs = require("fs");
 const axios = require('axios');
 const cheerio = require('cheerio');
-let testArray= [7984, 7994, 8029, 8007, 8025, 8030, 8031, 8018, 7999, 8016];
-let resultArray = []
-/*
+
+
+let testArray= [7984, 7994, 8029, 8007, 8025, 8030, 8031, 8018, 7999, 8016,
+  7989, 7996, 7982, 8008, 8023, 8019, 7985, 8005, 7988, 7990,
+  8013, 8022, 8026, 7995, 8012, 8000, 7992, 8024, 8021, 7986,
+  8020, 8001, 8006, 7983, 8009, 7993, 8002, 8010, 7991, 8014,
+  8075, 8084, 8073, 8036, 8054, 8040, 8070, 8056, 8077, 8069,
+  8042, 8035, 8048, 8049, 8050, 8041, 8037, 8079, 8083, 8043,
+  8033, 8066, 8076, 8067, 8038, 8074, 8071, 8078, 8044, 8034,
+  8065, 8080, 8047, 8072, 8045, 8052, 7886, 7884, 7881, 7913,
+  7911, 7905, 7915, 7921, 7927, 7928, 7902, 7883, 7887, 7888,
+  7914, 7885, 7889, 7882, 7926, 7890, 7903, 7916, 7923, 7930];
+
 const testRetrieval = () => {
   Promise.all(testArray.map(item => {
-    return getLocationDescription(item);
-  })).then(res => console.log(res)).catch(err => console.log(err))
+    setInterval(function() {
+      return getLocationDescription(item);
+    },500)
+  }))
+  .then(res => console.log(res))
+  .catch(err => console.log(err))
 }
-*/
 
-/*
+
+
 const getOnDisplayItems = () => {
   axios.get(`https://collectionapi.metmuseum.org/public/collection/v1/search?isOnView=true&q=*`)
     .then(response => {
@@ -21,7 +37,7 @@ const getOnDisplayItems = () => {
       console.log(err)
     })
 }
-*/
+
 const getLocationDescription = (objectId) => {
   return axios.get(`https://www.metmuseum.org/art/collection/search/${objectId}`)
       .then(response => {

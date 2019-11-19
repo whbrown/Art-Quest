36f930ce3e97cfb77a9ff0c0f23e7502a236d246
fabianschu <48454910+fabianschu@users.noreply.github.com>
2019-11-19 12:31:22 +0100

    scraper is scraping

--- scraper/scraper.js
+++ scraper/scraper.js
@@ -4,6 +4,7 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 
 
+/*
 let testArray= [7984, 7994, 8029, 8007, 8025, 8030, 8031, 8018, 7999, 8016,
   7989, 7996, 7982, 8008, 8023, 8019, 7985, 8005, 7988, 7990,
   8013, 8022, 8026, 7995, 8012, 8000, 7992, 8024, 8021, 7986,
@@ -14,52 +15,55 @@ let testArray= [7984, 7994, 8029, 8007, 8025, 8030, 8031, 8018, 7999, 8016,
   8065, 8080, 8047, 8072, 8045, 8052, 7886, 7884, 7881, 7913,
   7911, 7905, 7915, 7921, 7927, 7928, 7902, 7883, 7887, 7888,
   7914, 7885, 7889, 7882, 7926, 7890, 7903, 7916, 7923, 7930];
-
+  */
+ 
 let counter = 0;
-let arrayLength = testArray.length;
-
-var stream = fs.createWriteStream("itemLocationsDescriptions.txt", {flags:'a'});
-
-let testRetrieval = setInterval(() => {
-  getLocationDescription(testArray[counter])
-  counter +=1;
-  if (counter === arrayLength) {
-    stream.end();
-    clearInterval(testRetrieval);
-  }
-}, 1000);
 
 
 const getOnDisplayItems = () => {
-  axios.get(`https://collectionapi.metmuseum.org/public/collection/v1/search?isOnView=true&q=*`)
-    .then(response => {
-      let OnDisplayItemIds = response.data.objectIDs;
-      console.log(OnDisplayItemIds);
-    })
-    .catch(err => {
-      console.log(err)
-    })
+  return axios.get(`https://collectionapi.metmuseum.org/public/collection/v1/search?isOnView=true&q=*`)
+  .then(response => {
+    let OnDisplayItemIds = response.data.objectIDs;
+    console.log(OnDisplayItemIds);
+    let arrayLength = OnDisplayItemIds.length;
+    console.log(arrayLength);
+    
+    let retrieval = setInterval(() => {
+      getLocationDescription(OnDisplayItemIds[counter]);
+      counter +=1;
+      if (counter === arrayLength) {
+        stream.end();
+        clearInterval(retrieval);
+      }
+    }, 1000);
+  })
+  .catch(err => {
+    console.log(err)
+  })
 }
+
+var stream = fs.createWriteStream("itemLocationsDescriptions.txt", {flags:'a'});
 
 const getLocationDescription = (objectId) => {
   return axios.get(`https://www.metmuseum.org/art/collection/search/${objectId}`)
-      .then(response => {
-        if(response.status === 200) {
-          const html = response.data;
-          const $ = cheerio.load(html);
-          const description = $(".artwork__intro__desc").text()
-          const location =  $(".artwork__location--gallery").text()
-          stream.write(JSON.stringify({
-            objectId: objectId,
-            location: location,
-            description: description.replace(/^\s+|\s+$/g, '')
-          }) + ",");
-        }  
-      })
-      .catch(err => {
-        console.log(err)
-      })
+  .then(response => {
+    if(response.status === 200) {
+      const html = response.data;
+      const $ = cheerio.load(html);
+      const description = $(".artwork__intro__desc").text()
+      const location =  $(".artwork__location--gallery").text()
+      console.log(description, location);
+      stream.write(JSON.stringify({
+        objectId: objectId,
+        location: location,
+        description: description.replace(/^\s+|\s+$/g, '')
+      }) + "\n");
+    }  
+  })
+  .catch(err => {
+    console.log(err)
+  })
 }
 
 //getLocationDescription(`https://www.metmuseum.org/art/collection/search/436524`);
-//getOnDisplayItems()
+getOnDisplayItems();

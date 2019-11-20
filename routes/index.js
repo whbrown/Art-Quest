77a8a7e9e74c5e3ff3d209612523479e7792a9e5
William Brown <hce@WmHB.local>
2019-11-20 10:14:45 +0100

    Create route for assessment page and remove large csv & json datadumps

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,30 @@
 const express = require('express');
+const getSampleObjects = require('../evalPreferences/evalPreferences');
 
 const router = express.Router();
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+router.get('/assess/:medium', (req, res, next) => {
+  const media = {
+    painting: 'Paintings',
+    sculpture: 'Sculpture',
+    furniture: 'Furniture',
+    music: 'Musical+instruments',
+  };
+  const selectedMedia = media[req.params.medium];
+  Promise.all(getSampleObjects(selectedMedia))
+    .then(objects => {
+      res.render('assessment', { objects });
+    })
+    .catch(err => console.log(err));
+});
+
+router.post('/assess', (req, res, next) => {
+  req.body;
 });
 
 module.exports = router;

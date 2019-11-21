e043332577d0b0a48507b09a1a933222144f4ad8
William Brown <hce@WmHB.local>
2019-11-21 19:32:44 +0100

    change mongo-uri

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const MongoStore = require('connect-mongo')(session);
 const flash = require('connect-flash');
 
 mongoose
-  .connect('mongodb://localhost/art-quest', { useNewUrlParser: true })
+  .connect(process.env.MONGODB_URI || 'mongodb://localhost/art-quest', { useNewUrlParser: true })
   .then(x => {
     console.log(
       `Connected to Mongo! Database name: "${x.connections[0].name}"`

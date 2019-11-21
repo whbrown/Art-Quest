d41dafcef9d5880c4740e58570b0df0e94947392
William Brown <hce@WmHB.local>
2019-11-21 12:06:26 +0100

    Add cloister coverage for map

--- map/mapScript.js
+++ map/mapScript.js
@@ -6,6 +6,10 @@ function gotoMap(gallery) {
     }
     return arr;
   };
+  const cloisters1 = genRange(1, 4)
+    .concat(genRange(6, 8))
+    .concat(16, 20);
+  const cloistersG = genRange(9, 14);
   const level5Galleries = ['926'];
   const level3Galleries = genRange(708, 716)
     .concat(genRange(219, 222))
@@ -24,12 +28,19 @@ function gotoMap(gallery) {
     .concat(genRange(170, 172))
     .concat(genRange(773, 774));
   const levelGGalleries = genRange(980, 981).concat(genRange(963, 965));
-  if (level5Galleries.includes(gallery)) return `/images/met-map/MetMap5.png`;
-  if (level3Galleries.includes(gallery)) return `/images/met-map/MetMap3.png`;
-  if (level2Galleries.includes(gallery)) return `/images/met-map/MetMap2.png`;
-  if (level1MGalleries.includes(gallery)) return `/images/met-map/MetMap1M.png`;
-  if (levelGGalleries.includes(gallery)) return `/images/met-map/MetMapG.png`;
-  return `/images/met-map/MetMap1.png`;
+  if (cloisters1.includes(gallery)) return `/images/met-map/MetCloisters1.png`;
+  if (cloistersG.includes(gallery)) return `/images/met-map/MetCloistersG.png`;
+  if (level5Galleries.includes(gallery))
+    return `/images/met-map/Met5thAve5.png`;
+  if (level3Galleries.includes(gallery))
+    return `/images/met-map/Met5thAve3.png`;
+  if (level2Galleries.includes(gallery))
+    return `/images/met-map/Met5thAve2.png`;
+  if (level1MGalleries.includes(gallery))
+    return `/images/met-map/Met5thAve1M.png`;
+  if (levelGGalleries.includes(gallery))
+    return `/images/met-map/Met5thAveG.png`;
+  return `/images/met-map/Met5thAve1.png`;
 }
 
 module.exports = gotoMap;
